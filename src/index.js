8806a81b73cab8d29b4ca17e889ee0cd5bb13c43
Valeriia Kyrychenko <valeriia.kyrychenko22@gmail.com>
2023-01-21 00:44:57 +0200

    добавляет плавный скролл

--- src/index.js
+++ src/index.js
@@ -94,7 +94,17 @@ function onLoad() {
     }
 
  createMarkup(hits);
-     new SimpleLightbox('.gallery a').refresh();;
+    new SimpleLightbox('.gallery a').refresh();
+
+
+    const { height: cardHeight } = document
+  .querySelector(".gallery")
+  .firstElementChild.getBoundingClientRect();
+
+window.scrollBy({
+  top: cardHeight * 2,
+  behavior: "smooth",
+});
 
   }).catch(error => console.log('ERROR: ' + error))
 }
