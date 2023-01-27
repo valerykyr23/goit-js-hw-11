5bf01b369a88229d5f4e69619813675b8647a237
Valeriia Kyrychenko <valeriia.kyrychenko22@gmail.com>
2023-01-27 13:28:20 +0200

    added styles css and js for scroll-up button

--- src/index.js
+++ src/index.js
@@ -138,3 +138,39 @@ function createMarkup(value) {
 function clearAll() {
   refs.gallery.innerHTML = "";
 }
+
+// Код для скролл-кнопки
+
+
+const scrollUpBtn = document.querySelector('.scrollUpBtn');
+console.log(scrollUpBtn);
+const rootEl = document.documentElement;
+console.log(rootEl);
+
+// When scrolling on the website calling a function that calculates scrolled pixels
+onScrollFunction();
+window.onscroll = function () {
+  onScrollFunction();
+};
+
+// When scrolling more than 2500px class .showBtn either add or removed, which either reveal button or removes it
+function onScrollFunction() {
+  if (
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
+  ) {
+    scrollUpBtn.classList.add('showBtn');
+  } else {
+    scrollUpBtn.classList.remove('showBtn');
+  }
+}
+
+scrollUpBtn.addEventListener('click', scrollUpTop);
+
+// Function that scrolls the page up when clicked on the button
+function scrollUpTop() {
+  rootEl.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
